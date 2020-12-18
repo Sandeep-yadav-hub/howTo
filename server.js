7ef16b1a9072e5b9a37501ba52c71249ded44412
pkyad <pkyisky@gmail.com>
2020-12-18 13:15:11 +0530

    upload

--- server.js
+++
@@ -1,9 +0,0 @@
-
-// import app from app.js 
-const app = require('./expressapp')
-
-//  Set Port
-const port = process.env.PORT ;
-app.listen(port, () => console.log('server is running on port : http://localhost:' + port))
-
-
